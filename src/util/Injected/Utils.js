92120dca5b20eca3aba326e2656c36b578837b61
Gary Webb <2311498+garywebb@users.noreply.github.com>
2025-11-13 16:09:59 +0000

    Test fix

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -1,5 +1,37 @@
 'use strict';
 
+class CancellationToken {
+    constructor() {
+        this._isCancelled = false;
+        this.cancelCallbacks = [];
+    }
+
+    get isCancelled() {
+        return this._isCancelled;
+    }
+
+    cancel() {
+        this._isCancelled = true;
+        this.cancelCallbacks.forEach(cb => cb());
+    }
+
+    onCancel(callback) {
+        if (this.isCancelled) {
+            callback();
+        } else {
+            this.cancelCallbacks.push(callback);
+        }
+    }
+
+    throwIfCancelled() {
+        if (this._isCancelled) {
+        throw new CancelledError("Operation cancelled");
+        }
+    }
+}
+
+export class CancelledError extends Error {}
+
 exports.LoadUtils = () => {
     window.WWebJS = {};
 
